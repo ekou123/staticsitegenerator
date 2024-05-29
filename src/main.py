4e4e516e2b0f8cbcd790b74fc85cdd7fdd0a7599
Ethan Kouris <ethankouris321@gmail.com>
2024-05-29 17:05:23 +1000

    Updated main.py file

--- src/main.py
+++ src/main.py
@@ -6,6 +6,8 @@ def main():
     test = TextNode("This is text node", "bold", "https://www.bootdev.com")
     test2 = TextNode("This is a text node", "bold", "https://www.bootdev.com")
 
+    print("Hello world!")
+
 
 if __name__ == "__main__":
     main()
