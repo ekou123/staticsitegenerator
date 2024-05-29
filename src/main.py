4fd5f45616eefa141928dec0308b40a95b87c00b
Ethan Kouris <ethankouris321@gmail.com>
2024-05-29 16:54:14 +1000

    Added textnode.py with TextNode class

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
-print("Hello world")
+from textnode import TextNode
+
+
+
+def main():
+    test = TextNode("This is text node", "bold", "https://www.bootdev.com")
+
+    print(test.__repr__)
+
+
+if __name__ == "__main__":
+    main()
 
