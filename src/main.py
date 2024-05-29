a771869780ff77859e5791105547af2c18bdcf30
Ethan Kouris <ethankouris321@gmail.com>
2024-05-29 17:00:29 +1000

    Fixed ubug in main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from textnode import TextNode
 
 
 def main():
-    test = TextNode("This is text node", "bold", "https://www.bootdev.com")gi
+    test = TextNode("This is text node", "bold", "https://www.bootdev.com")
 
 
 if __name__ == "__main__":
