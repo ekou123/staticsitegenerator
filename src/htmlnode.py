9b0ecfa0d9a704e38287a7f84e1152fc1403ca96
Ethan Kouris <ethankouris321@gmail.com>
2024-05-29 17:41:02 +1000

    Added LeadNode class to htmlnode.py and created test cases for the to_html function inside that class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,3 +20,17 @@ class HTMLNode:
 
     def __repr__(self):
         return f'{self.tag}, {self.value}, {self.children}, {self.props}'
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("No value variable found")
+        if self.tag is None:
+            return ""
+        
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,13 +4,29 @@ from htmlnode import *
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props_to_html(self):
+    def test_props_to_html_htmlnode(self):
         htmlnode = HTMLNode(None, None, None, {"href": "https://www.google.com", "target": "_blank"})
 
         actual = htmlnode.props_to_html()
         expected = ' href="https://www.google.com" target="_blank"'
         
         self.assertEqual(actual, expected)
+
+    def test_to_html_required_values_only_leafnode(self):
+        leafnode = LeafNode("p", "This is a paragraph of text.")
+
+        expected = '<p>This is a paragraph of text.</p>'
+        actual = leafnode.to_html()
+
+        self.assertEqual(expected, actual)
+    
+    def test_to_html_all_values_leafnode(self):
+        leafnode = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+
+        expected = '<a href="https://www.google.com">Click me!</a>'
+        actual = leafnode.to_html()
+
+        self.assertEqual(expected, actual)
 
 
 
